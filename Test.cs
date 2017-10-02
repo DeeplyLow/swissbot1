709381cb7e58f797c10deb301db7ea3418a51687
DeeplyLow <gabrielmadina@gmail.com>
2017-10-02 16:15:39 -0300

    Add files via upload

---
+++ Test.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Swiss2.Modules
+{
+    public class Test : ModuleBase<SocketCommandContext>
+    {
+        [Command("botstatus")]
+        public async Task fjvidfjv()
+        {
+            await Context.Channel.SendMessageAsync("This discord bot is up and running!")
+        }
+    }
+}
